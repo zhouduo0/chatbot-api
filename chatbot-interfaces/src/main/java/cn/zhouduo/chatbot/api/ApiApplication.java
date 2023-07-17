12575b0520b7d8aa7b9cfa1ab50342469427ddcc
zhouduo <zhouduo@udesk.cn>
2023-07-17 14:41:34 +0800

    Initial commit

---
+++ chatbot-interfaces/src/main/java/cn/zhouduo/chatbot/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package cn.zhouduo.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouduo
+ * @date 2023/7/17
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
